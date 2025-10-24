b9eb3dd046845c0fb56fc5dd42954dc4d61092d3
Kamil <fikama290@vp.pl>
2025-10-24 02:10:16 +0200

    Bump version to 1.0.4 and add Python 3.8-3.12 support

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 from setuptools import setup
 from setuptools import find_packages
 
-version = "1.0.3"
+version = "1.0.4"
 
 with open('README.md') as f:
     long_description = f.read()
@@ -45,6 +45,11 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Security',
         'Topic :: System :: Installation/Setup',
